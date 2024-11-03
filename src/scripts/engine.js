efebae401b6fe8eab1876ca90b023cb7cfdfc868
BruhQuartz <135336260+BruhQuartz@users.noreply.github.com>
2024-11-03 15:56:29 -0300

    Add files via upload

---
+++ src/scripts/engine.js
@@ -0,0 +1,58 @@
+const emojis = [
+    "🎂",
+    "🎂",
+    "😍",
+    "😍",
+    "🌹",
+    "🌹",
+    "💋",
+    "💋",
+    "🤦🏻‍♂️",
+    "🤦🏻‍♂️",
+    "👻",
+    "👻",
+    "🧙🏽‍♀️",
+    "🧙🏽‍♀️",
+    "🤘🏻",
+    "🤘🏻"
+];
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for (let index = 0; index < emojis.length; index++) {
+    let box = document.createElement("div");
+    box.className = "item";
+    box.onclick = handleClick;
+    box.innerHTML = shuffleEmojis[index];
+    document.querySelector(".game").appendChild(box);
+}
+
+function handleClick() {
+    if (openCards.length < 2) {
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if (openCards.length == 2) {
+        setTimeout(checkMatch, 500);
+    }
+}
+
+function checkMatch() {
+    if (openCards[0].innerHTML == 
+        openCards[1].innerHTML) {
+            openCards[0].classList.add("boxMatch");
+            openCards[1].classList.add("boxMatch");
+    }
+    else {
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
+    }
+
+    openCards = [];
+    if (document.querySelectorAll(".boxMatch").length == 
+        emojis.length) {
+            alert('Você venceu!')
+    }
+}
